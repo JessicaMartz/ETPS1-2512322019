d455030e29b8180950ca86e01cab23bd1c971fbc
Jessica Martinez <2512322019@mail.utec.edu.sv>
2023-02-12 23:50:26 -0600

    G1
    12022023

---
+++ G1/app/src/main/java/sv/edu/utec/g1/MainActivity.java
@@ -0,0 +1,30 @@
+package sv.edu.utec.g1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+EditText txtNombres;
+Button btnIngreso;
+TextView etinfo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtNombres=findViewById(R.id.txtNombre);
+        etinfo=findViewById(R.id.etInfor);
+
+    }
+    public void Nom(View view){
+        String nomb;
+        nomb=txtNombres.getText().toString();
+        etinfo.setText("Bienvenido"+" "+ nomb);
+    }
+}
+
